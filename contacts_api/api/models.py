ec719d7a7f785dfec81eb554103a07f9be3ab588
mudjaycker <tombutoyi@gmail.com>
2022-10-22 09:43:35 +0200

    gigignore

--- contacts_api/api/models.py
+++ contacts_api/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=26, unique=True)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    @classmethod
+    def default(cls):
+        group, created = cls.objects.get_or_create(name="No Group")
+        return group.name
+    
+    def __str__(self):
+        return str(self.name)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=26, null=False, blank=False)
+    number = models.CharField(max_length=26, null=False, blank=False, unique=True)
+    favorite = models.BooleanField(default=False)
+    group = models.ForeignKey(Group, to_field="name", default=Group.default, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.number}"
