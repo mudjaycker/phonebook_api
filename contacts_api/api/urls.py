dd78abbfbff26866c3da76f31fd91473a31ea0eb
mudjaycker <tombutoyi@gmail.com>
2022-10-25 05:57:09 +0200

    adding yasg

--- contacts_api/api/urls.py
+++ contacts_api/api/urls.py
@@ -1,8 +1,27 @@
 from rest_framework import routers
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt import views as jwt_views
 from .views import *
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 router = routers.DefaultRouter()
 router.register("Group", GroupViewSet)
@@ -13,6 +32,11 @@ urlpatterns = [
     path("", include(router.urls)),
     path('login/', TokenPairView.as_view()),
     path("api-auth/", include("rest_framework.urls")),
-     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
+    
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
